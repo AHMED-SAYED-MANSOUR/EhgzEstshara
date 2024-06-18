60a771b76522684a43d99ead434cb70f7db21bd0
hady-asaker <hadyasaker8@gmail.com>
2024-06-18 22:21:05 +0300

    one to one relation with cart

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
 }
