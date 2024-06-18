96f8db2fec2fca19fecb1f524310b4845470fcc0
hady-asaker <hadyasaker8@gmail.com>
2024-06-19 01:18:48 +0300

    ADD some columns

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,6 +10,6 @@ class Product extends Model
     use HasFactory;
     use HasFactory;
     protected $table = 'products';
-    protected $fillable = ['id' , 'ProductName' , 'Quantity' , 'Price', 'Category', 'Description', 'img', 'offer'];
+    protected $fillable = ['id' , 'ProductName' , 'Quantity' , 'Price', 'Category', 'Description', 'img', 'offer','Brand', 'Material', 'Color', 'created_at', 'updated_at'];
 
 }
