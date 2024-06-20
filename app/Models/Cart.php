e4821253377cc02242da7ae4672329a89ef515aa
hady-asaker <hadyasaker8@gmail.com>
2024-06-20 22:34:59 +0300

    Cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -26,4 +26,5 @@ class Cart extends Model
         return $this->belongsTo(Product::class);
     }
 
+
 }
--- resources/views/partials/products.blade.php
+++ resources/views/partials/products.blade.php
@@ -21,8 +21,8 @@
                     @endif
 {{--                    <p class="text-primary">{{ $product->Category }}</p>--}}
                     <div class="team-social text-center">
-                        <a style="padding-right: 10px" class="btn btn-square" href="{{ Auth::check() ? route('cart') : route('sign') }}"><i class="fa-solid fa-cart-shopping"></i></a>
-                        <a class="btn btn-square" href="{{ Auth::check() ? route('cart') : route('sign') }}"><i class="fa fa-heart"></i></a>
+                        <a style="padding-right: 10px" class="btn btn-square" href="{{ Auth::check() ? route('cart.index') : route('sign') }}"><i class="fa-solid fa-cart-shopping"></i></a>
+                        <a class="btn btn-square" href="{{ Auth::check() ? route('cart.index') : route('sign') }}"><i class="fa fa-heart"></i></a>
                     </div>
                 </div>
             </div>
