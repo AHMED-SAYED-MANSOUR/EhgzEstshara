b3eccee29f8ade1c6057027ed18ba7cf5a16037a
hady-asaker <hadyasaker8@gmail.com>
2024-06-20 06:31:32 +0300

    text-align:center for "Heading"

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -69,7 +69,7 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
-            <h1 class="display-3 text-white mb-3 animated slideInDown">Doctors</h1>
+            <h1 class="display-3 text-white mb-3 animated slideInDown" style="text-align: center">Doctors</h1>
             <nav aria-label="breadcrumb animated slideInDown">
                  <!-- <ol class="breadcrumb text-uppercase mb-0">
                     <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
