debfc1d92ccb3e4971797b4cf656f915d5f0797e
hady-asaker <hadyasaker8@gmail.com>
2024-06-22 08:30:01 +0300

    Admin Trainer Control

--- resources/views/admin/trainer/create.blade.php
+++ resources/views/admin/trainer/create.blade.php
@@ -1,38 +1,55 @@
 @extends('dashboard')
 @section('title')
-create new offers
+    Create Trainer
 @endsection
 @section('content')
-    <!-- <h1>create new offers</h1>
-     -->
-    <div class="card">
-      <div class="card-header">
-        <a href="{{url('show-trainers')}}" class="btn btn-success">Show Trainers</a>
-      </div>
-  <div class="card-body">
+    <div class="container mt-5">
+        <h1>Create Trainer</h1>
 
-    @if(count($errors) > 0)
-    <div class="alert alert-danger">
-      @foreach($errors->all() as $error)
-         <li>{{ $error }}</li>
-    @endforeach
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('admin.trainer.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+
+            <div class="form-group mb-3">
+                <label for="name">Name</label>
+                <input type="text" name="name" class="form-control" id="name" >
+            </div>
+
+            <div class="form-group mb-3">
+                <label for="title">Title</label>
+                <input type="text" name="title" class="form-control" id='title' >
+            </div>
+
+            <div class="form-group mb-3">
+                <label for="image">Image</label>
+                <input type="file" name="image" class="form-control" id="image">
+            </div>
+
+            <div class="form-group mb-3">
+                <label for="facebook">Facebook</label>
+                <input type="url" name="facebook" class="form-control" id="facebook">
+            </div>
+
+            <div class="form-group mb-3">
+                <label for="instagram">Instagram</label>
+                <input type="url" name="instagram" class="form-control" id="instagram">
+            </div>
+
+            <div class="form-group mb-3">
+                <label for="twitter">Twitter</label>
+                <input type="url" name="twitter" class="form-control" id="twitter">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Create Trainer</button>
+        </form>
     </div>
-    @endif
-
-    <form method="post" action="{{url('save')}}" enctype="multipart/form-data">
-      {{csrf_field()}}
-        <label>Trainer Name:</label>
-
-        <input type="text" name="name" placeholder="Trainer Name" class="form-control mb-3 py-4"  value="{{old('name')}}">
-        <label>Trainer title:</label>
-
-        <input type="text" name="title" placeholder="Trainer Title" class="form-control  mb-3 py-4">
-        <label>Trainer photo:</label>
-
-        <input type="file" name="photo" placeholder="Trainer photo" class="form-control mb-3 px-4 pt-3 pb-5">
-        <div class="text-center"><input type='submit' value=submit class="btn btn-primary mt-4"></div>
-    </form>
-  </div>
-</div>
-
 @endsection
