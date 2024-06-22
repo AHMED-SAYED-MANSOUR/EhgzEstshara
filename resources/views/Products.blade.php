ae5b2e6ffc3c58f0a769c8e2b3b9e79fb423cd5d
AHMED-SAYED-MANSOUR <117373675+AHMED-SAYED-MANSOUR@users.noreply.github.com>
2024-06-22 04:16:33 +0300

    Cart Quantity and Products offers

--- resources/views/Products.blade.php
+++ resources/views/Products.blade.php
@@ -16,6 +16,7 @@
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
+
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet">
 
